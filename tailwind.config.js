b5e337a24a7ac5abc0150db649520707a91ab9cf
ItzHardcore <bruno.vp.1999@gmail.com>
2021-12-15 21:50:05 +0000

    start

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: "jit",
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        70: "70vh",
+      },
+      fontSize: {
+        headings: "3.25rem",
+        body: "1.25rem",
+        footer: "2.1875rem",
+        SecondFooter: "0.68rem",
+      },
+      padding: {
+        10: "2.5rem",
+        213: "21vh",
+      },
+      width: {
+        500: "53vh",
+      },
+    },
+  },
+  plugins: [],
+};
